106ed7f8323503a9e954b19bd004bf298418ba61
Enrico Monese <enricomonese@gmail.com>
2018-02-03 22:09:32 +0100

    Very bad commit description

--- monete.js
+++ monete.js
@@ -14,41 +14,38 @@ program
   .version('0.1.0')
   .option('-c, --copy', 'Copies time to clipboard at the end')
   .option('-p, --precise', 'Show exact time passed as [0h 0m]')
-  .option('-s, --same-screen, 'Don\'t output to alternate screen')
+  .option('-s, --same-screen', 'Don\'t output to alternate screen')
   .parse(process.argv);
 
 var elapsed = 0;
 
 co(function *() {
-  //Begin
-  if(program.sameScreen)
+  if(!program.sameScreen) {
     show();
-  process.stdout.write("\033c");
+  }
   console.log(chalk.bold.blue(format('Begin tracking time at {}', moment().format('kk:mm'))));
   if(program.copy) {
     console.log("Will copy time in format [0h 0m] to clipboard at the end");
   }
   console.log('\nPress enter to stop\n');
 
-  const spinner = ora().start();
+  const spinner = ora('just now').start();
   var timer = setInterval(() => {
     elapsed++;
     spinner.color = 'green';
     spinner.text = time(program.precise);
   }, 1000);
-
-  //wait for user to press enter to stop timer
   yield prompt('\r');
-
-  //handle results
-  spinner.succeed(chalk.bold.blue(format("Tracked time [{}]",time())));
+  process.stdout.write("\033c");
+  show();
+  spinner.succeed(chalk.bold.blue(format("Tracked time [{}]",time(program.precise))));
   if(program.copy) {
     var t = time(true);
     clipboardy.writeSync(t);
-    console.log(chalk.bold(format("Copied time [{}] to clipboard!", t)));
+    console.log(chalk.bold("Copied time to clipboard!"));
   }
-  if(program.sameScreen) {
-    yield prompt('Press enter again to exit');
+  if(!program.sameScreen) {
+    yield prompt('\nPress enter again to exit');
     hide();
   }
   process.exit(1);
@@ -59,7 +56,7 @@ function time (precise = false) {
   if(precise) {
     if(d.hours() > 0) {
       return format("{}h {}m", d.hours(), d.minutes());
-    } if(d.minutes() > 0) {
+    } else if(d.minutes() > 0) {
       return format("{}m", d.minutes());
     }
   }
